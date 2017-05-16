a30a65ce5e5dbd81e4b7d459dc0e2b01d8e87efb
brandonvin <studentuser@D-173-250-180-235.dhcp4.washington.edu>
2017-05-15 18:55:28 -0700

    add w8 skeleton

--- js/applykatex.js
+++ js/applykatex.js
@@ -1,6 +1,6 @@
 (function(document) {
     var maths = document.querySelectorAll(".katex-math");
-    var opts = {displayStyle: true};
+    var opts = {displayMode: true};
     for (var i = 0; i < maths.length; i++) {
         katex.render(maths[i].textContent, maths[i], opts);
     }
