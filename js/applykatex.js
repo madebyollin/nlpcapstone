24f2877f7de7f5737f561b96502139bf0873d167
Irving Chen <irfing@users.noreply.github.com>
2017-05-16 23:10:00 +0000

    w8 / Tidy up math

--- js/applykatex.js
+++ js/applykatex.js
@@ -1,7 +1,27 @@
+/*global katex */
+
 (function(document) {
-    var maths = document.querySelectorAll(".katex-math");
-    var opts = {displayMode: true};
+    var maths, opts;
+
+    // "Deprecated"
+    maths = document.querySelectorAll(".katex-math");
+    opts = {displayMode: true};
     for (var i = 0; i < maths.length; i++) {
         katex.render(maths[i].textContent, maths[i], opts);
     }
+
+
+    // Use these two going forward!
+    maths = document.querySelectorAll(".math-display");
+    opts = {displayMode: true};
+    for (var i = 0; i < maths.length; i++) {
+        katex.render(maths[i].textContent, maths[i], opts);
+    }
+
+    maths = document.querySelectorAll(".math-inline");
+    for (var i = 0; i < maths.length; i++) {
+        katex.render(maths[i].textContent, maths[i]);
+    }
+
+
 }(document));
